997bbacaab614de2dc751360ba00f5769f239819
Simhyejin <hj12334@naver.com>
2016-08-18 15:25:26 +0900

    Update CFLoginResponse.cs

--- CF/CFLoginResponse.cs
+++ CF/CFLoginResponse.cs
@@ -1,11 +1,12 @@
-    struct LoginResponseBody
-    {
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 15)]
-        char[] ip;
-        int port;
-        ushort socketType; 
-        
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
-        char[] cookie;
- 
-    }
+//Login and ConnectPassing
+struct LoginResponseBody
+{
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 15)]
+    char[] ip;
+    int port;
+    ushort socketType; 
+    
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
+    char[] cookie;
+
+}
