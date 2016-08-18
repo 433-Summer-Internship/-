6a12f6d4e6aec731378aa1c759ce40ed6bd018a0
Simhyejin <hj12334@naver.com>
2016-08-18 15:33:46 +0900

    Create FBLoginRequest.cs

---
+++ FB_Protocol/FBLoginRequest.cs
@@ -0,0 +1,7 @@
+struct FBLoginRequest
+{
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 12)]
+    char[] user;
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 18)]
+    char[] password;
+}
