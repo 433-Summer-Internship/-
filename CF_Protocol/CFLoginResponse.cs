f2ab9fbb3f7c562aea2fd576b25f94ab4e6d9a9f
Simhyejin <hj12334@naver.com>
2016-08-18 17:04:05 +0900

    Update CFLoginResponse.cs

--- CF_Protocol/CFLoginResponse.cs
+++ CF_Protocol/CFLoginResponse.cs
@@ -4,7 +4,6 @@ struct CFLoginResponse
     [MarshalAs(UnmanagedType.ByValArray, SizeConst = 15)]
     char[] ip;
     int port;
-    ushort socketType; 
     
     [MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
     char[] cookie;
