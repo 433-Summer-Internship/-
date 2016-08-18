b1a188c6833f3f2f8b6f6deecbbbc7c3af53b7f6
Simhyejin <hj12334@naver.com>
2016-08-18 15:16:10 +0900

    Create CFLoginResponse.cs

---
+++ CF/CFLoginResponse.cs
@@ -0,0 +1,11 @@
+    struct LoginResponseBody
+    {
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 15)]
+        char[] ip;
+        int port;
+        ushort socketType; 
+        
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
+        char[] cookie;
+ 
+    }
